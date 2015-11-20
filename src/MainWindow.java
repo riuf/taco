74eb6ca85006b9bf7a8dfc87aaa91fbcff7ea271
Lapteva Kseniya <ksyu.lapteva@yandex.ru>
2015-11-20 12:32:13 +0900

    Login and MainWindow added.

---
+++ src/MainWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by User16 on 20.11.2015.
+ */
+public class MainWindow extends JFrame{
+    public MainWindow(){
+        super();
+
+        this.setVisible(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
